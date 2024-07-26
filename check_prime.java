f8651bde2e480161e03992c0cb1d3eb05452d9ed
Nhaaa4 <162309557+Nhaaa4@users.noreply.github.com>
2024-07-26 22:13:47 +0700

    first commit

---
+++ check_prime.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class check_prime {
+  public static void main(String[] args) {
+    boolean flag = false;
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter number");
+    int num = myObj.nextInt();
+
+    if (num < 2) {
+      System.out.println(num + "is not a prime number");
+      myObj.close();
+      return;
+    }
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+
+    myObj.close();
+  }
+}
